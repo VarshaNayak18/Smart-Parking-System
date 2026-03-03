2320f58b581c70952b45770d72cd854960f28ce9
Varsha V Nayak <varshavnayak2003@gmail.com>
2026-03-03 10:04:53 +0530

    Created vehicle object in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Smart Parking System");
+        
+        // Creating a vehicle object
+        Vehicle v1 = new Vehicle("KA01AB1234", "Car");
+        
+        System.out.println("Vehicle Number: " + v1.getVehicleNumber());
+        System.out.println("Vehicle Type: " + v1.getVehicleType());
+        System.out.println("Entry Time: " + v1.getEntryTime());
     }
 }
